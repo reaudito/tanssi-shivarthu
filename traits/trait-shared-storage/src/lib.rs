719605a406b6c635d148cfde30345515dc54bcb8
Amiya Behera <amiyatulu@gmail.com>
2024-05-24 11:07:08 +0530

    dispatch result error

--- traits/trait-shared-storage/src/lib.rs
+++ traits/trait-shared-storage/src/lib.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+use frame_support::pallet_prelude::DispatchResult;
 
 pub trait SharedStorageLink {
 	type AccountId;
 
-	fn check_citizen_is_approved_link(address: Self::AccountId) -> DispatchResult;
+	fn check_citizen_is_approved_link(address: Self::AccountId) ->  DispatchResult;
 
 	fn get_approved_citizen_count_link() -> u64;
 	fn set_positive_externality_link(address: Self::AccountId, score: i64) -> DispatchResult;
