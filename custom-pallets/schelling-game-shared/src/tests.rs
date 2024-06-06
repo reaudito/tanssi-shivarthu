baff40adf1404484cb710edbd013fac335ce4382
Amiya Behera <amiyatulu@gmail.com>
2024-06-06 16:35:24 +0530

    score schelling game test

--- custom-pallets/schelling-game-shared/src/tests.rs
+++ custom-pallets/schelling-game-shared/src/tests.rs
@@ -830,3 +830,130 @@ fn score_schelling_game_test() {
 		assert_eq!(299625, balance);
 	});
 }
+
+
+#[test]
+fn score_schelling_game_value_test() {
+	new_test_ext().execute_with(|| {
+		let key = return_key_profile(0);
+		let now = 10;
+		assert_ok!(TemplateModule::set_to_evidence_period(key.clone(), now));
+		assert_eq!(TemplateModule::get_period(&key).unwrap(), Period::Evidence);
+		let game_type = return_game_type_profile_approval();
+		// let min_short_block_length = return_min_short_block_length();
+		// let min_long_block_length = return_min_long_block_length();
+		let phase_data = get_the_phase_data();
+
+		let staking_start_time = now + phase_data.staking_length;
+		assert_ok!(TemplateModule::set_to_staking_period(
+			key.clone(),
+			phase_data.clone(),
+			staking_start_time
+		));
+		// Create tree
+		assert_ok!(TemplateModule::create_tree_link_helper(key.clone(), 3));
+		// Check the period is staking
+		let period = TemplateModule::get_period(key.clone());
+		// println!("{:?}", period);
+		assert_eq!(Some(Period::Staking), period);
+		// Applyjuror
+		for j in 4..30 {
+			assert_ok!(TemplateModule::apply_jurors_helper(
+				key.clone(),
+				phase_data.clone(),
+				j,
+				j * 100
+			));
+		}
+		let new_now = staking_start_time + phase_data.staking_length;
+		assert_ok!(TemplateModule::change_period(key.clone(), phase_data.clone(), new_now.clone()));
+		let period = TemplateModule::get_period(key.clone());
+		assert_eq!(Some(Period::Drawing), period);
+		assert_ok!(TemplateModule::draw_jurors_helper(key.clone(), phase_data.clone(), 5));
+		let draws_in_round = TemplateModule::draws_in_round(key.clone());
+		assert_eq!(5, draws_in_round);
+		let drawn_jurors = TemplateModule::drawn_jurors(key.clone());
+		assert_eq!(vec![(4, 400), (7, 700), (13, 1300), (14, 1400), (15, 1500)], drawn_jurors);
+		assert_ok!(TemplateModule::change_period(key.clone(), phase_data.clone(), new_now.clone()));
+		let balance = Balances::free_balance(5);
+		assert_eq!(299500, balance);
+		assert_ok!(TemplateModule::unstaking_helper(key.clone(), 5));
+		let balance = Balances::free_balance(5);
+		assert_eq!(300000, balance);
+		let hash = sp_io::hashing::keccak_256("1salt".as_bytes());
+		assert_ok!(TemplateModule::commit_vote_for_score_helper(key.clone(), 4, hash));
+		let hash = sp_io::hashing::keccak_256("1salt2".as_bytes());
+		assert_ok!(TemplateModule::commit_vote_for_score_helper(key.clone(), 7, hash));
+		let hash = sp_io::hashing::keccak_256("5salt3".as_bytes());
+		assert_ok!(TemplateModule::commit_vote_for_score_helper(key.clone(), 13, hash));
+		let hash = sp_io::hashing::keccak_256("1salt4".as_bytes());
+		assert_ok!(TemplateModule::commit_vote_for_score_helper(key.clone(), 14, hash));
+		let hash = sp_io::hashing::keccak_256("7salt5".as_bytes());
+		assert_ok!(TemplateModule::commit_vote_for_score_helper(key.clone(), 15, hash));
+		let commit_start_time = TemplateModule::commit_start_time(key.clone());
+		let new_now = commit_start_time + phase_data.commit_length;
+		assert_ok!(TemplateModule::change_period(key.clone(), phase_data.clone(), new_now.clone()));
+		let period = TemplateModule::get_period(key.clone());
+		assert_eq!(Some(Period::Vote), period);
+		assert_ok!(TemplateModule::reveal_vote_score_helper(
+			key.clone(),
+			4,
+			1,
+			"salt".as_bytes().to_vec()
+		));
+		assert_ok!(TemplateModule::reveal_vote_score_helper(
+			key.clone(),
+			7,
+			1,
+			"salt2".as_bytes().to_vec()
+		));
+		assert_ok!(TemplateModule::reveal_vote_score_helper(
+			key.clone(),
+			13,
+			5,
+			"salt3".as_bytes().to_vec()
+		));
+		assert_ok!(TemplateModule::reveal_vote_score_helper(
+			key.clone(),
+			14,
+			1,
+			"salt4".as_bytes().to_vec()
+		));
+		assert_noop!(
+			TemplateModule::reveal_vote_score_helper(
+				key.clone(),
+				15,
+				8,
+				"salt5".as_bytes().to_vec()
+			),
+			Error::<Test>::CommitDoesNotMatch
+		);
+		assert_ok!(TemplateModule::reveal_vote_score_helper(
+			key.clone(),
+			15,
+			7,
+			"salt5".as_bytes().to_vec()
+		));
+		let vote_start_time = TemplateModule::vote_start_time(key.clone());
+		let new_now = vote_start_time + phase_data.commit_length;
+		assert_ok!(TemplateModule::change_period(key.clone(), phase_data.clone(), new_now.clone()));
+		let period = TemplateModule::get_period(key.clone());
+		assert_eq!(Some(Period::Execution), period);
+		let reveal_score = TemplateModule::reveal_score_values(key.clone());
+		assert_eq!(vec![1000, 1000, 5000, 1000, 7000], reveal_score);
+		assert_ok!(TemplateModule::set_new_mean_value(key.clone()));
+		let mean_values = TemplateModule::new_mean_reveal_score(key.clone());
+		assert_eq!(2000, mean_values.unwrap());
+		let result = TemplateModule::get_score_value_schelling(key.clone(), 4, RangePoint::ZeroToTen);
+		assert_eq!(result.unwrap(), WonLost::Won);
+		let result = TemplateModule::get_score_value_schelling(key.clone(), 7, RangePoint::ZeroToTen);
+		assert_eq!(result.unwrap(), WonLost::Won);
+		let result = TemplateModule::get_score_value_schelling(key.clone(), 13, RangePoint::ZeroToTen);
+		assert_eq!(result.unwrap(), WonLost::Lost);
+		let result = TemplateModule::get_score_value_schelling(key.clone(), 14, RangePoint::ZeroToTen);
+		assert_eq!(result.unwrap(), WonLost::Won);
+		let result = TemplateModule::get_score_value_schelling(key.clone(), 15, RangePoint::ZeroToTen);
+		assert_eq!(result.unwrap(), WonLost::Lost);
+	});
+}
+
